1904d237f9d4f4f28149a99b4d0d3f984d4d6185
Roy <71298965+suhaash02@users.noreply.github.com>
2020-11-20 07:07:51 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,59 +1,38 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+from pyrogram import Client,filters
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+app_id = 'your api hash'
+app_key = 'your api id'
+token = "api token"
 
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-""" MtProto Bot """
 
-from pyrogram import (
-    Client,
-    __version__
-)
-from bot import (
-    API_HASH,
-    APP_ID,
-    LOGGER,
-    TG_BOT_TOKEN,
-    TG_BOT_WORKERS
+app = Client("antiservicemessage", app_id, app_key, bot_token=token)
+
+DONATESTARTTEXT = """
+text  
+"""
+
+
+
+
+@app.on_message(filters.private)
+def start(c,m):
+    m.reply(DONATESTARTTEXT,
 )
 
+@app.on_message(filters.group & filters.command("command@botname"))
+def main(c,m):
+    m.reply("""text""")
 
-class Bot(Client):
-    """ modded client for SessionMakerBot """
+@app.on_message(filters.group & filters.command("command"))
+def main(c,m):
+    m.reply("""text""")
 
-    def __init__(self):
-        super().__init__(
-            "SessionMakerBot",
-            api_hash=API_HASH,
-            api_id=APP_ID,
-            bot_token=TG_BOT_TOKEN,
-            plugins={
-                "root": "bot/plugins"
-            },
-            workers=TG_BOT_WORKERS
-        )
-        self.LOGGER = LOGGER
 
-    async def start(self):
-        await super().start()
-        usr_bot_me = await self.get_me()
-        self.set_parse_mode("html")
-        self.LOGGER(__name__).info(
-            f"@{usr_bot_me.username} based on Pyrogram v{__version__} "
-            "Try /start."
-        )
 
-    async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("SessionMakerBot stopped. Bye.")
+@app.on_message(filters.service)
+def service(c,m):
+    m.delete()
+
+
+app.run()
